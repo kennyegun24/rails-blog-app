e2f508c6e94e46f5e9d06bdcf70ded56d28c0aca
jaamanu <kennyegun240@gmail.com>
2023-02-08 20:23:47 +0100

    Tests for controllers

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   get "users/:user_id/posts", to: "posts#index", as: "user_posts"
-  get "users/user_id/posts/:id", to: "posts#show", as: "user_post"
+  get "users/:user_id/posts/:id", to: "posts#show", as: "user_post"
   get "users", to: "users#index" 
   get "users/:id", to: "users#show", as: "user"
 end
