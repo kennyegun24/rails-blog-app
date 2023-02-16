1b311925378f3f79550f99e1e9889ee5d41232fb
jaamanu <kennyegun240@gmail.com>
2023-02-16 11:35:04 +0100

    Fixed linter errors

--- spec/systems/user/show_spec.rb
+++ spec/systems/user/show_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe 'Posts index view', type: :system do
   describe 'Index page' do
     before(:each) do
-      @user = User.create(Name: 'Kenny', Photo: 'https://img.freepik.com/free-psd/save-world-reusable-grocery-bag-mockup_53876-98827.jpg?', Bio: 'Kenny elias')
+      @user = User.create(Name: 'Kenny',
+                          Photo: 'https://img.freepik.com/free-psd/save-world-reusable-grocery-bag-mockup_53876-98827.jpg?', Bio: 'Kenny elias')
       @post = Post.create(text: 'ken', title: 'ken', author: @user)
       Comment.create(post: @post, author: @user, text: 'Amazing post Tom')
       Comment.create(post: @post, author: @user, text: 'Awesome post Tom')
@@ -13,28 +14,28 @@ RSpec.describe 'Posts index view', type: :system do
     end
 
     it 'Should have a post titkle' do
-      expect("#{page.current_url}/#{@post.id}").to match users_path(@user.id) 
+      expect("#{page.current_url}/#{@post.id}").to match users_path(@user.id)
     end
 
     it 'expect page to have user profile photo' do
-      expect(page).to have_css("img[src*='#{@user.Photo}']") 
+      expect(page).to have_css("img[src*='#{@user.Photo}']")
     end
 
     it 'expect username to display' do
-      expect(page).to have_content(@user.Name)  
+      expect(page).to have_content(@user.Name)
     end
 
     it '' do
-      expect(page).to have_content(@user.PostCounter)  
+      expect(page).to have_content(@user.PostCounter)
     end
 
     it '' do
-      expect(page).to have_content(@user.Bio) 
+      expect(page).to have_content(@user.Bio)
     end
 
     it '' do
       @user.posts.each do |comment|
-        expect(page).to have_content(comment.text)  
+        expect(page).to have_content(comment.text)
       end
     end
 
@@ -43,11 +44,11 @@ RSpec.describe 'Posts index view', type: :system do
     end
 
     it do
-      expect("#{page.current_url}/posts").to match user_posts_path(@user.id) 
+      expect("#{page.current_url}/posts").to match user_posts_path(@user.id)
     end
 
     it do
-      expect("#{page.current_url}/posts/#{@post.id}").to match user_posts_path(@user, @post) 
+      expect("#{page.current_url}/posts/#{@post.id}").to match user_posts_path(@user, @post)
     end
   end
 end
